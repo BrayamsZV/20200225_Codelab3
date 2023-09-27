ef300add37394b067d2b0ce47b57065f96a19caa
BrayamsZV <brayams.zacarias@unmsm.edu.pe>
2023-09-27 16:48:56 -0500

    proyecto de codelab3

---
+++ art-space-main/app/src/main/java/dev/codeninja/artspace/MainActivity.kt
@@ -0,0 +1,209 @@
+package dev.codeninja.artspace
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import dev.codeninja.artspace.ui.theme.ArtSpaceTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			ArtSpaceTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+					ArtSpaceScreen()
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun ArtSpaceScreen(modifier: Modifier = Modifier) {
+	val firstArtwork = R.drawable.coche1
+	val secondArtwork = R.drawable.coche2
+	val thirdArtwork = R.drawable.coche3
+	val fourthArtwork = R.drawable.coche4
+
+	var title by remember {
+		mutableStateOf(R.string.coche1)
+	}
+
+	var model by remember {
+		mutableStateOf(R.string.coche1_model)
+	}
+
+	var currentArtwork by remember {
+		mutableStateOf(firstArtwork)
+	}
+
+	var imageResource by remember {
+		mutableStateOf(currentArtwork)
+	}
+
+
+	Column(
+		modifier = modifier
+			.fillMaxWidth(),
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		ArtworkDisplay(currentArtwork = currentArtwork)
+		Spacer(modifier = modifier.size(16.dp))
+		ArtworkTitle(title = title, model = model)
+		Spacer(modifier = modifier.size(25.dp))
+		Row(
+			modifier = modifier.padding(horizontal = 8.dp),
+			horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally)
+		) {
+			// Previous Button
+			Button(
+				onClick = {
+					when (currentArtwork) {
+						firstArtwork -> {
+							currentArtwork = fourthArtwork
+							title = R.string.coche4
+							model = R.string.coche4_model
+						}
+						secondArtwork -> {
+							currentArtwork = firstArtwork
+							title = R.string.coche1
+							model = R.string.coche1_model
+						}
+						thirdArtwork -> {
+							currentArtwork = secondArtwork
+							title = R.string.coche2
+							model = R.string.coche2_model
+						}
+						else -> {
+							currentArtwork = thirdArtwork
+							title = R.string.coche3
+							model = R.string.coche3_model
+						}
+					}
+				},
+				colors = ButtonDefaults.buttonColors(
+					backgroundColor = colorResource(id = R.color.gray_900)
+				),
+				elevation = ButtonDefaults.elevation(
+					defaultElevation = 1.dp,
+					pressedElevation = 0.dp,
+					focusedElevation = 0.dp
+				)
+			) {
+				Text(
+					text = "Previous",
+					fontSize = 16.sp,
+					fontWeight = FontWeight.Medium,
+					color = colorResource(id = R.color.blue_300)
+				)
+			}
+
+			// Next Button
+			Button(
+				onClick = {
+					when (currentArtwork) {
+						firstArtwork -> {
+							currentArtwork = secondArtwork
+							title = R.string.coche2
+							model = R.string.coche2_model
+						}
+						secondArtwork -> {
+							currentArtwork = thirdArtwork
+							title = R.string.coche3
+							model = R.string.coche3_model
+						}
+						thirdArtwork -> {
+							currentArtwork = fourthArtwork
+							title = R.string.coche4
+							model = R.string.coche4_model
+						}
+						else -> {
+							currentArtwork = firstArtwork
+							title = R.string.coche1
+							model = R.string.coche1_model
+						}
+					}
+				},
+				colors = ButtonDefaults.buttonColors(
+					backgroundColor = colorResource(id = R.color.blue_300)
+				),
+				elevation = ButtonDefaults.elevation(
+					defaultElevation = 1.dp,
+					pressedElevation = 0.dp,
+					focusedElevation = 0.dp
+				)
+			) {
+				Text(
+					text = "Next",
+					fontSize = 16.sp,
+					fontWeight = FontWeight.Medium,
+					color = colorResource(id = R.color.gray_900)
+				)
+			}
+		}
+	}
+}
+
+@Composable
+fun ArtworkDisplay(
+	modifier: Modifier = Modifier,
+	@DrawableRes currentArtwork: Int
+) {
+	Image(
+		painter = painterResource(currentArtwork),
+		contentDescription = stringResource(id = R.string.coche2),
+		modifier = modifier.fillMaxWidth(),
+		contentScale = ContentScale.FillWidth
+	)
+}
+
+@Composable
+fun ArtworkTitle(
+	@StringRes title: Int,
+	@StringRes model: Int
+) {
+	Column (
+		horizontalAlignment = Alignment.CenterHorizontally
+	) {
+		// Artwork title
+		Text(
+			text = stringResource(id = title),
+			fontWeight = FontWeight.Bold,
+			color = colorResource(id = R.color.blue_100),
+			fontSize = 32.sp
+		)
+		
+		// Artwork year
+		Text(
+			text = "— ${stringResource(id = model)} —",
+			fontSize = 16.sp,
+			fontWeight = FontWeight.Medium,
+			color = colorResource(id = R.color.gray_300)
+		)
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+	ArtSpaceTheme {
+		ArtSpaceScreen()
+	}
+}
